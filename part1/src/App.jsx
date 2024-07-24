591f6a06d02e592afb55455995c7dee075fd3a9c
ChuaZiLong <122143961+ChuaZiLong@users.noreply.github.com>
2024-07-24 13:23:20 +0800

    Complete exercise 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,29 +1,30 @@
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <h1>{course.name}</h1>
-      <p>{course.parts[0].name} {course.parts[0].exercises}</p>
-      <p>{course.parts[1].name} {course.parts[1].exercises}</p>
-      <p>{course.parts[2].name} {course.parts[2].exercises}</p>
-      <p>Number of exercises {course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises}</p>
+      <h1>give feedback</h1>
+      <Button onClick={()=>setGood(good + 1)} text='good'/>
+      <Button onClick={()=>setNeutral(neutral + 1)} text='neutral'/>
+      <Button onClick={()=>setBad(bad+1)} text='bad'/>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good + neutral + bad) / 3}</p>
+      <p>positive {good / (good + neutral + bad) * 100} %</p>
     </div>
   )
 }

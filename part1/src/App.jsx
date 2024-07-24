37b97741fc4c49d62944ede460b4894fec8b2d78
ChuaZiLong <122143961+ChuaZiLong@users.noreply.github.com>
2024-07-24 13:38:56 +0800

    Complete exercise 1.8

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,6 +6,18 @@ const Button = ({onClick, text}) => (
   </button>
 )
 
+const Statistics = ({good, neutral, bad}) => (
+  <div>
+    <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good + neutral + bad) / 3}</p>
+      <p>positive {good / (good + neutral + bad) * 100} %</p>
+  </div>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -18,13 +30,7 @@ const App = () => {
       <Button onClick={()=>setGood(good + 1)} text='good'/>
       <Button onClick={()=>setNeutral(neutral + 1)} text='neutral'/>
       <Button onClick={()=>setBad(bad+1)} text='bad'/>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good + neutral + bad) / 3}</p>
-      <p>positive {good / (good + neutral + bad) * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad = {bad}/>
     </div>
   )
 }
